bea3df225efa40d7b6ee2742a40c3162bf2e33f2
Mirzoulugbek <bilmadim.uz@gmail.com>
2021-05-27 18:21:13 +0900

    Date pickeer and Http delete

--- src/components/Main/InsertPopup/InsertPop.js
+++ src/components/Main/InsertPopup/InsertPop.js
@@ -1,9 +1,23 @@
-import React, { useRef } from 'react'
+import React, { useRef, useImperativeHandle, useState } from 'react'
 import classes from './InsertPop.module.css'
+import Radio from '@material-ui/core/Radio';
+import { withStyles } from '@material-ui/core/styles';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
 
 const Input = React.forwardRef((props, ref) => {
     const inputRef = useRef()
-    console.log(props.value)
+
+    const activate = () => {
+        inputRef.current.focus()
+    }
+
+    useImperativeHandle(ref, () => {
+        return {
+            focus: activate,
+        }
+    })
+
     return (
         <div className={classes.control}>
             <label htmlFor={props.id}>{props.label}</label>
@@ -14,10 +28,22 @@ const Input = React.forwardRef((props, ref) => {
                 id={props.id}
                 value={props.value}
                 placeholder={props.placeholder}
+                onChange={props.onChange}
             ></input>
         </div>
     )
 })
+
+const GreenRadio = withStyles({
+    root: {
+        color: 'rgb(180, 133, 72)',
+        '&$checked': {
+            color: 'rgb(180, 133, 72)',
+        },
+    },
+    checked: {},
+})((props) => <Radio color="default" {...props} />);
+
 
 function InsertPop() {
 
@@ -32,6 +58,9 @@ function InsertPop() {
     const insDatetimeRef = useRef()
     const useYNRef = useRef()
 
+    let Ins_DateTime = ""
+    let Ups_DateTime = ""
+
     const insertTemp = {
         Ins_Emp: "Aaaa",
         Up_Emp: "ddddd",
@@ -39,12 +68,20 @@ function InsertPop() {
         Item_Name: "xxxxxxx",
         Item_Spec: "444444",
         Remark: "ddddddd",
-        Unit_Code: "122222222", 
+        Ins_DateTime: Ins_DateTime,
+        Ups_DateTime: Ups_DateTime,
+        Unit_Code: "122222222",
         Use_YN: {
             Y: false,
             N: true
         }
     }
+
+    const [selectedValue, setSelectedValue] = React.useState(true);
+
+    const handleChange = (event) => {
+        setSelectedValue(!selectedValue);
+    };
 
     const submitCreatedItem = (event) => {
         event.preventDefault()
@@ -77,19 +114,40 @@ function InsertPop() {
         })
     }
 
+    const UpDateTimeHandler = (event) => {
+        Ups_DateTime = event.target.value
+    }
+
     return (
         <div className={classes.Popup}>
             <form onSubmit={submitCreatedItem}>
                 <Input ref={insEmpRef} label="Ins_Emp" />
                 <Input ref={upEmpRef} label="Up_Emp" />
-                <Input ref={itemCodeRef} label="Item_Code" />
+                <Input type="number" ref={itemCodeRef} label="Item_Code" />
                 <Input ref={itemNameRef} label="Item_Name" />
                 <Input ref={itemSpecRef} label="Item_Spec" />
                 <Input ref={remarkRef} label="Remark" />
                 <Input ref={unitCodeRef} label="Unit_Code" />
-                <Input ref={upDatetimeRef} label="Up_DateTime" />
-                <Input ref={insDatetimeRef} label="Ins_DateTime" />
-                <Input ref={useYNRef} label="Use_YN" />
+
+                <Input type="date" onChange={UpDateTimeHandler} label="Up_DateTime" />
+                <Input type="date" label="Ins_DateTime" />
+
+                <div className={classes.radio}>
+                    <label>Use_YN</label>
+                    <div>
+                        <FormControlLabel labelPlacement="end" value="true" control={<GreenRadio
+                            checked={selectedValue}
+                            onChange={handleChange}
+                            name="radio-button-demo"
+                        />} label="Yes" />
+                        <FormControlLabel labelPlacement="end" value="false" control={<GreenRadio
+                            checked={!selectedValue}
+                            onChange={handleChange}
+                            name="radio-button-demo"
+                        />} label="No" />
+                    </div>
+                </div>
+
                 <button className={classes.button}>Insert</button>
             </form>
         </div>
